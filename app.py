2f771ed9fa5ebde9799452530f2251c37b1099de
Adrian <zjoaquin@uninorte.edu.co>
2021-10-27 11:52:59 -0500

    Rutas

--- app.py
+++ app.py
@@ -6,4 +6,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def login():
-    return render_template('ListadoPeliculas.html') 
+    return render_template('Login.html') 
+
+@app.route('/administrador')
+def Administardor():
+    return render_template('Dashboard.html') 
+
+
+@app.route('/registro')
+def Registrar():
+    return render_template('RegistrarUsuario.html')
+
+@app.route('/registro/Perfilusuario')
+def Perfil_Usuario():
+    return render_template('PerfilUsuario.html')
+
+@app.route('/listapeliculas')
+def Lista_Peliculas():
+    return render_template('ListadoPeliculas.html')  
+
+@app.route('/login/listadopeliculas/funcion')
+def Detalle_funcion():
+    return render_template('Detalles.html') 
+
+
+@app.route('/login/listadopeliculas/busquedad')
+def Busquedad():
+    return render_template('ResultadoBusquedad.html')
+    
